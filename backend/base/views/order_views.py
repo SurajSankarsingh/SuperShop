f5608072b9ec9fcdd303eeecd60f4f073cbcad3f
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-05-24 17:30:44 -0400

    {feat: Backend - added updateOrderToPaid to order Views and urls)

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -8,6 +8,7 @@ from base.models import Product, Order, OrderItem, ShippingAddress
 from base.serializers import ProductSerializer, OrderSerializer
 
 from rest_framework import status
+from datetime import datetime
 
 
 @api_view(['POST'])
@@ -72,3 +73,15 @@ def getOrderById(request, pk):
       Response({'detail': 'Not Authorized to view this order'},status=status.HTTP_400_BAD_REQUEST)
   except:
     return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request, pk):
+  order = Order.objects.get(_id=pk)
+
+  order.isPaid = True
+  order.paidAt = datetime.now()
+  order.save()
+  
+  return Response('Order was paid')
