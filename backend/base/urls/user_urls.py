ebe8f85a1fa8138306020b79866b30be43c2d452
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-05-25 02:31:38 -0400

    (feat: Backend - setup delete user Views and urls. Frontend - added delete user Actions, Reducer and store, also setup functionality on user list screen to allow admin to delete user with confirmation)

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('profile/', views.getUserProfile, name="user-profile"),
     path('profile/update/', views.updateUserProfile, name="user-profile-update"),
     path('', views.getUsers, name="user"),
+    
+    path('delete/<str:pk>/', views.deleteUser, name="user-delete"),
 ]
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-import { listUsers } from '../actions/userActions';
+import { listUsers, deleteUser } from '../actions/userActions';
 
 function UserListScreen({ history }) {
   const dispatch = useDispatch();
@@ -15,16 +15,21 @@ function UserListScreen({ history }) {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
+
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
       dispatch(listUsers());
     } else {
       history.push('/login');
     }
-  }, [dispatch, history]);
+  }, [dispatch, history, userInfo, successDelete]);
 
   const deleteHandler = (id) => {
-    console.log('Delete:', id);
+    if (window.confirm('Are you sure you want to delete this user?')) {
+      dispatch(deleteUser(id));
+    }
   };
 
   return (
