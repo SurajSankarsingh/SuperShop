1057cec75d4a3d73a9ae4805e47fa2b24b5fe076
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-05-26 18:38:29 -0400

    (feat: Backend - added create and update product views and urls, also modified database to load a placeholder image if none is provided)

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -3,7 +3,11 @@ from base.views import product_views as views
 
 urlpatterns = [
     path('', views.getProducts, name="products"),
+    
+    path('create/', views.createProduct, name="product-create"),
     path('<str:pk>/', views.getProduct, name="product"),
     
-    path('delete/<str:pk>/', views.deleteProduct, name="delete-product"),
+    path('update/<str:pk>/', views.updateProduct, name="product-update"),
+    
+    path('delete/<str:pk>/', views.deleteProduct, name="product-delete"),
 ]
