cc959d817cda280711d99ea34ae6f13a800dcd08
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-05-29 03:53:22 -0400

    (chore: Frontend - removed unused dependencies)

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
@@ -37,7 +37,7 @@ function PlaceOrderScreen({ history }) {
       history.push(`/order/${order._id}`);
       dispatch({ type: ORDER_CREATE_RESET });
     }
-  }, [success, history]);
+  }, [success, history, dispatch]);
 
   const placeOrder = () => {
     dispatch(
